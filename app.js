12f43a786da606df8e6eb0f78f031b6da84fb4d0
gorgonzola1 <81741626+gorgonzola1@users.noreply.github.com>
2025-06-02 04:17:49 -0300

    added response for /

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/', (req, res) => {
+  res.send('¡API funcionando correctamente!');
+});
+
+module.exports = app;
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
